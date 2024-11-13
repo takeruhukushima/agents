0252f97606570a714b786b16588931ad5c7c9800
takeru.fukushima <100330935+takeruhukushima@users.noreply.github.com>
2024-11-13 15:41:38 +0900

    1113

--- examples/minimal_worker.py
+++ examples/minimal_worker.py
@@ -1,6 +1,9 @@
 import logging
 
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, WorkerType, cli
+from dotenv import load_dotenv
+load_dotenv()
+
 
 logger = logging.getLogger("my-worker")
 logger.setLevel(logging.INFO)
